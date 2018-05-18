b41829d8076ed103f0a019cffacf22246c1eb904
Real man born on semtember <hieunam03@gmail.com>
2018-05-18 12:34:47 +0700

    Dot bringup

--- dot.mk
+++ dot.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
+$(call inherit-product, device/samsung/gprimelte-common/dot.mk)
 
 $(call inherit-product, device/samsung/gprimeltespr/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gprimeltespr
-PRODUCT_NAME := lineage_gprimeltespr
+PRODUCT_NAME := dot_gprimeltespr
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G530P
 PRODUCT_MANUFACTURER := samsung
